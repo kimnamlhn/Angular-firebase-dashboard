286a10e710969969fd2580e62fe2f1b7c6d61ad6
kimnamlhn <kimnam.cpc@gmail.com>
2021-09-13 00:22:30 +0700

    firebase database api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDJRAD_iIEmQjyYXJzY0d_GEaSv0tF06FI",
+    authDomain: "dashboard-86c09.firebaseapp.com",
+    projectId: "dashboard-86c09",
+    storageBucket: "dashboard-86c09.appspot.com",
+    messagingSenderId: "823686575444",
+    appId: "1:823686575444:web:31150c6faf4271545944c1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDJRAD_iIEmQjyYXJzY0d_GEaSv0tF06FI",
+    authDomain: "dashboard-86c09.firebaseapp.com",
+    projectId: "dashboard-86c09",
+    storageBucket: "dashboard-86c09.appspot.com",
+    messagingSenderId: "823686575444",
+    appId: "1:823686575444:web:31150c6faf4271545944c1"
+  }
 };
 
 /*
